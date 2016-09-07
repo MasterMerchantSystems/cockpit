8b99fe67588f659333ff36ff04fa2515978e134d
Chris Wiseman <chris.wiseman@mmspos.com>
2016-09-07 17:57:24 -0300

    KING-856: Modified api response to return image thumbnails along with fullsize images

--- modules/core/Collections/Controller/RestApi.php
+++ modules/core/Collections/Controller/RestApi.php
@@ -3,6 +3,10 @@
 namespace Collections\Controller;
 
 class RestApi extends \LimeExtra\Controller {
+    private function pre($str){
+        echo '<pre>',print_r($str,1),'</pre>';
+    }
+
     public function getBySlug($collection=null) {
         return $this->get($collection, true);
     }
@@ -30,15 +34,28 @@ class RestApi extends \LimeExtra\Controller {
             if ($limit  = $this->param("limit", null))  $options["limit"] = $limit;
             if ($sort   = $this->param("sort", null))   $options["sort"] = $sort;
             if ($skip   = $this->param("skip", null))   $options["skip"] = $skip;
-            
+
             if (count($options)) {
                 $options = json_decode(json_encode($options, JSON_NUMERIC_CHECK), true);
             }
 
             $entries = $this->app->db->find("collections/{$col}", $options);
+            $entries_array = $entries->toArray();
+            $modified_entries = array();
+            foreach($entries_array as $entry){
+                $modified_entry = array();
+                foreach($entry as $key => $value){
+                    if(stripos($key, 'image') !== false){
+                        $path_to_thumbnail = cockpit('mediamanager:thumbnail', $value, 50, 50, ['mode' => 'best_fit']);
+                        $modified_entry[$key . '_thumbnail'] = $path_to_thumbnail;
+                    }
+                    $modified_entry[$key] = $value;
+                }
+                $modified_entries[] = $modified_entry;
+            }
         }
 
-        return json_encode($entries->toArray());
+        return json_encode($modified_entries);
     }
 
 }
